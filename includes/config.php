46728f4268557b2ec044ee6c2f7206830b06e66d
Vidit Tiwari <dev.vidit@gmail.com>
2015-06-18 20:51:37 +0530

    config file

---
+++ includes/config.php
@@ -0,0 +1,27 @@
+<?php
+ob_start();
+session_start();
+
+//database credentials
+define('DBHOST','localhost');
+define('DBUSER','database username');
+define('DBPASS','password');
+define('DBNAME','database name');
+
+//application address
+define('DIR','http://domain.com/');
+define('SITEEMAIL','noreply@domain.com');
+
+try {
+
+	//create PDO connection 
+	$db = new PDO("mysql:host=".DBHOST.";port=3306;dbname=".DBNAME, DBUSER, DBPASS);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+	//show error
+    echo '<p class="bg-danger">'.$e->getMessage().'</p>';
+    exit;
+}
+
+?>
